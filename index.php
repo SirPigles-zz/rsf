7913c794b02e3928080b01949d69de9b607cec10
Kuriji <nichvannice@outlook.com>
2015-10-22 18:33:31 -0600

    Suggestion to <title>

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!doctype html>
 <head>
-  <title>Raffle Smash Website</title>
+  <title>Welcome to Raffle Smash!</title>/* Suggestion
+  <title>Raffle Smash Website</title>*/
   <script src="/jquery.js"></script>
 </head>
-<body>
+
+<body> 
   <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
